4813979c66c61e2d66e8dc950dc69d1fdb2763f8
Samu <samu.uunonen@student.hamk.fi>
2022-02-14 20:26:17 +0200

    form submit message
    fixed message

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Form from "./components/Form/Form";
 import Table from "./components/Table/Table";
@@ -9,6 +9,18 @@ function App() {
   const [persons, setPersons] = useState<Array<object>>([]);
   const [error, setError] = useState<string | boolean>(false);
   const [succesfull, setSuccesfull] = useState<string | boolean>(false);
+  const [formMessageClass, setFormMessageClass] = useState<string>("form-message");
+  const [message, setMessage] = useState<string | boolean>("");
+
+  useEffect(() => {
+    if (error !== false) {
+      setFormMessageClass("form-message error")
+      setMessage(error);
+    } else if (succesfull !== false) {
+      setFormMessageClass("form-message success")
+      setMessage(succesfull);
+    }
+  }, [error, succesfull])
 
   return (
     <div className="App">
@@ -20,10 +32,7 @@ function App() {
           setError={setError}
           setSuccessfull={setSuccesfull}
         />
-        {error !== false && <p className="error">{error}</p>}
-        {succesfull !== false && (
-          <p className="success" data-testid="succesfull-message">{succesfull}</p>
-        )}
+        <p className={formMessageClass} data-testid="form-message">{message}</p>
       </div>
       <div className="table-container">
         {persons.length > 0 ? (
